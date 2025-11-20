4d5b4eed78a48c13e4ebfbd9b497664deca78d68
arppy <berta@inf.u-szeged.hu>
2025-11-20 08:46:33 +0100

    get lenght of wavs

---
+++ get_length_of_wav.py
@@ -0,0 +1,29 @@
+import torchaudio
+from pathlib import Path
+import argparse
+import params
+
+parser = argparse.ArgumentParser(description="Evaluation with ASR.")
+parser.add_argument("dataset", metavar="dataset", type=str, help="Name of dataset.")
+parser.add_argument("wav_dir", metavar="wav-dir", type=Path, help="path to audio directory.")
+args = parser.parse_args()
+
+wav_files = list(Path(args.wav_dir).glob("*.wav"))
+
+all_length_per_class = {}
+for i in range(params.label_count[args.dataset]) :
+    all_length_per_class[i] = 0
+for i in range(0, len(wav_files)):
+    wav_file = wav_files[i]
+    label = int(wav_file.stem.split("_")[1]) - 1
+    try:
+        waveform, sample_rate = torchaudio.load(str(wav_file))
+        duration = waveform.shape[1] / sample_rate
+        all_length_per_class[label] += duration
+    except Exception as e:
+        print(f"Error processing {wav_file}: {e}")
+for label in range(params.label_count[args.dataset]) :
+    if all_length_per_class[label] == 0 :
+        print(-1)
+    else:
+        print(all_length_per_class[label])
