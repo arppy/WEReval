9b25dd3c33cf31f84688bb2c92a00a5f0981316b
arppy <berta@inf.u-szeged.hu>
2025-11-20 11:02:13 +0100

    get length for SzegedDys

--- get_length_of_wav.py
+++ get_length_of_wav.py
@@ -2,6 +2,7 @@ import torchaudio
 from pathlib import Path
 import argparse
 import params
+from utils import SzegedDys_label_to_idx
 
 parser = argparse.ArgumentParser(description="Evaluation with ASR.")
 parser.add_argument("dataset", metavar="dataset", type=str, help="Name of dataset.")
@@ -15,7 +16,12 @@ for i in range(params.label_count[args.dataset]) :
     all_length_per_class[i] = 0
 for i in range(0, len(wav_files)):
     wav_file = wav_files[i]
-    label = int(wav_file.stem.split("_")[1]) - 1
+    wav_file_name_parts = wav_file.stem.split("_")
+    label = 0
+    if args.dataset == params.HUNDYS :
+        label = int(wav_file_name_parts[1]) - 1
+    elif args.dataset == params.SZEGEDYS :
+        label = SzegedDys_label_to_idx[wav_file_name_parts[0]]
     try:
         waveform, sample_rate = torchaudio.load(str(wav_file))
         duration = waveform.shape[1] / sample_rate
