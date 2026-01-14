b57eca416f763b6422a3c1464ce9a1bec518bf1e
arppy <berta@inf.u-szeged.hu>
2026-01-14 17:09:14 +0100

    debug - whisper eval

--- eval_with_whisper_for_longer_wav.py
+++ eval_with_whisper_for_longer_wav.py
@@ -166,19 +166,11 @@ all_wer_per_class = {}
 all_wN_per_class = {}
 all_cer_per_class = {}
 all_cN_per_class = {}
-average_wer_per_class = []
-average_cer_per_class = []
-all_transcriptions_str_per_class = {}
-all_expects_str_per_class = {}
 for i in range(params.label_count[args.dataset]) :
     all_wer_per_class[i] = []
     all_wN_per_class[i] = []
     all_cer_per_class[i] = []
     all_cN_per_class[i] = []
-    all_transcriptions_str_per_class[i] = ""
-    all_expects_str_per_class[i] = ""
-    average_wer_per_class.append(-1)
-    average_cer_per_class.append(-1)
 for idx, row in enumerate(results):
     lab = int(row["class_label"])
     wer = float(row["wer"])
@@ -189,17 +181,11 @@ for idx, row in enumerate(results):
     all_wN_per_class[lab].extend([word_N])  # Add the current batch's WERs to the list
     all_cer_per_class[lab].extend([cer])  # Add the current batch's CERs to the list
     all_cN_per_class[lab].extend([char_N])  # Add the current batch's CERs to the list
-    average_wer_per_class[lab] = np.mean(all_wer_per_class[lab])
-    average_cer_per_class[lab] = np.mean(all_cer_per_class[lab])
 
-wer_a_list = []
 wer_w_list = []
-cer_a_list = []
 cer_w_list = []
 for lab in range(params.label_count[args.dataset]) :
-    if all_transcriptions_str_per_class[lab] != "" :
-        wer_a_list.append(metric_wer.compute(predictions=[all_transcriptions_str_per_class[lab]], references=[all_expects_str_per_class[lab]]))
-        cer_a_list.append(metric_cer.compute(predictions=[all_transcriptions_str_per_class[lab]], references=[all_expects_str_per_class[lab]]))
+    if all_wer_per_class[lab]:
         np_all_wer_per_lab = np.array(all_wer_per_class[lab])
         np_all_wN_per_lab = np.array(all_wN_per_class[lab])
         np_all_cer_per_lab = np.array(all_cer_per_class[lab])
@@ -207,11 +193,8 @@ for lab in range(params.label_count[args.dataset]) :
         wer_w_list.append(np.average(np_all_wer_per_lab, weights=np_all_wN_per_lab))
         cer_w_list.append(np.average(np_all_cer_per_lab, weights=np_all_cN_per_lab))
     else :
-        wer_a_list.append(-1.0)
         wer_w_list.append(-1.0)
-        cer_a_list.append(-1.0)
         cer_w_list.append(-1.0)
-print(wer_a_list, cer_a_list)
 print(wer_w_list, cer_w_list)
 print("average_wer_per_class")
 print(*wer_w_list, sep='\n')
